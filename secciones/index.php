d8d42aaf54ca8a24f17d180f35ff2338a46b9993
Bryan Saragosin <alexsaragosin5@gmail.com>
2023-06-28 22:44:13 -0500

    cambio de titulo

--- secciones/index.php
+++ secciones/index.php
@@ -3,7 +3,7 @@
 <div class="p-5 mb-4 bg-light rounded-3">
     <div class="container-fluid py-5">
       <h1 class="display-5 fw-bold">Bienvenidos a la aplicación </h1>
-      <p class="col-md-8 fs-4">Chupalo.....</p>
+      <p class="col-md-8 fs-4">CERTIFICADOS</p>
       <a class="btn btn-primary btn.lg" href="vista_alumnos.php" role="button">Iniciar</a>
     </div>
   </div>
